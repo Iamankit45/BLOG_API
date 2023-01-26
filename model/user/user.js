a812e68fb5c02a872a85f8ad25c5d51b207a9e2c
ANKIT <vikashkumar@ankit.local>
2023-01-27 00:38:17 +0530

    added basic schema

---
+++ model/user/user.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+
+const userSchema=new mongoose.Schema({
+
+    
+        
+          firstname: {
+            type: String,
+            required: [true, "First Name is required"],
+          },
+          lastname: {
+            type: String,
+            required: [true, "Last Name is required"],
+          },
+          profilePhoto: {
+            type: String,
+          },
+          email: {
+            type: String,
+            required: [true, "Email is required"],
+          },
+          password: {
+            type: String,
+            required: [true, "Password is required"],
+          },
+      
+          isBlocked: {
+            type: Boolean,
+            default: false,
+          },
+          isAdmin: {
+            type: Boolean,
+            default: false,
+          },
+          role: {
+            type: String,
+            enum: ["Admin", "Guest", "Editor"],
+          },
+          viewers: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "User",
+            },
+          ],
+          followers: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "User",
+            },
+          ],
+          following: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "User",
+            },
+          ],
+      
+          posts: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Post",
+            },
+          ],
+          comments: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Comment",
+            },
+          ],
+          blocked: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "User",
+            },
+          ]
+         
+          
+
+
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
