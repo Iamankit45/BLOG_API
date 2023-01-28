06f035d05a68b07634999fa0f127341e16cc3182
ANKIT <kumarankit0327@gmail.com>
2023-01-29 01:27:07 +0530

    hide .env file

--- model/user/user.js
+++ model/user/user.js
@@ -1,83 +1,99 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema=new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First Name is required"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last Name is required"],
+    },
+    profilePhoto: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
 
-    
-        
-          firstName: {
-            type: String,
-            required: [true, "First Name is required"],
-          },
-          lastName: {
-            type: String,
-            required: [true, "Last Name is required"],
-          },
-          profilePhoto: {
-            type: String,
-          },
-          email: {
-            type: String,
-            required: [true, "Email is required"],
-          },
-          password: {
-            type: String,
-            required: [true, "Password is required"],
-          },
-      
-          isBlocked: {
-            type: Boolean,
-            default: false,
-          },
-          isAdmin: {
-            type: Boolean,
-            default: false,
-          },
-          role: {
-            type: String,
-            enum: ["Admin", "Guest", "Editor"],
-          },
-          viewers: [
-            {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: "User",
-            },
-          ],
-          followers: [
-            {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: "User",
-            },
-          ],
-          following: [
-            {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: "User",
-            },
-          ],
-      
-          posts: [
-            {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: "Post",
-            },
-          ],
-          comments: [
-            {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: "Comment",
-            },
-          ],
-          blocked: [
-            {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: "User",
-            },
-          ]
-         
-          
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "Guest", "Editor"],
+    },
+    viewers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+    blocked: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    plan: [
+      {
+        type: String,
+        enum: ["Free", "Premium", "Pro"],
+        default: "Free",
+      },
+    ],
+
+userAward:[
+  {
+    type: String,
+    enum: ["Bronze", "Silver", "Gold"],
+    default: "Bronze",
+  },
+]
 
 
-})
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
