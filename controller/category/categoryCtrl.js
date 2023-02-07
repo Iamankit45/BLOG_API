fbb1de1eccd97c6868a04c5044f965ae555c21a2
ANKIT <kumarankit0327@gmail.com>
2023-02-07 21:42:22 +0530

    create category

--- controller/category/categoryCtrl.js
+++ controller/category/categoryCtrl.js
@@ -1,12 +1,20 @@
+
+const Category=require('../../model/category/category')
+const isLogin = require('../../middlewares/isLogin');
+const appErr = require('../../utils/appErr');
+
 //POST/api/v1/category/
- const registerCategoryCtrl= async (req, res) => {
+ const registerCategoryCtrl= async (req, res,next) => {
+
+  const {title}=req.body;
     try {
+      const category = await Category.create({title,user:req.userAuth});
       res.json({
         status: "success",
-        data: "category registered",
+        data: category
       });
     } catch (error) {
-      res.json(error.message);
+      return next(appErr(error.message));
     }
   }
   
--- routes/categories/categoryRoutes.js
+++ routes/categories/categoryRoutes.js
@@ -6,10 +6,12 @@ const {
   deleteCategoryCtrl,
   updateCategoryCtrl,
 } = require("../../controller/category/categoryCtrl");
+
+const isLogin=require("../../middlewares/isLogin")
 const categoryRouter = express.Router();
 
 //POST/api/v1/category/
-categoryRouter.post("/", registerCategoryCtrl);
+categoryRouter.post("/",isLogin,registerCategoryCtrl);
 
 //GET/api/v1/category/:id
 categoryRouter.get("/:id", getCategoryCtrl);
