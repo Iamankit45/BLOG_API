837aa1b13b468e1283103e73755b0ce08070d826
ANKIT <kumarankit0327@gmail.com>
2023-03-13 23:28:52 +0530

    modified model

--- model/user/user.js
+++ model/user/user.js
@@ -82,13 +82,13 @@ const userSchema = new mongoose.Schema(
       },
     ],
 
-    // plan: [
-    //   {
-    //     type: String,
-    //     enum: ["Free", "Premium", "Pro"],
-    //     default: "Free",
-    //   },
-    // ],
+    plan: [
+      {
+        type: String,
+        enum: ["Free", "Premium", "Pro"],
+        default: "Free",
+      },
+    ],
 
     userAward: {
       type: String,
