e889a593c6e6c9631840925250ababf639a5e20c
ANKIT <kumarankit0327@gmail.com>
2023-03-13 23:37:04 +0530

    edited post model

--- model/post/post.js
+++ model/post/post.js
@@ -3,62 +3,73 @@ const mongoose=require('mongoose');
 
 const postSchema=new mongoose.Schema({
 
-    title: {
-        type: String,
-        required: [true, "Post Title is required"],
-        trim: true,
-      },
-      description: {
-        type: String,
-        required: [true, "Post description is required"],
-      },
-      category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Category",
-        // required: [true, "Post category is required"],
-      },
-      numViews: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
-        },
-      ],
-      likes: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
-        },
-      ],
-      disLikes: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
-        },
-      ],
-  
-      comments: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Comment",
-        },
-      ],
-      user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: [true, "Please Author is required"],
-      },
-      photo: {
-        type: String,
-        required: [true, "Post Image is required"],
-      },
-    },
+  title: {
+    type: String,
+    required: [true, "Post Title is required"],
+    trim: true,
+  },
+  subtitle: {
+    type: String,
+    required: [true, "Post Title is required"],
+  },
+  minute_read: {
+    type: String,
+    required: [true, "Post Title is required"],
+  },
+  content: {
+    type: String,
+    required: [true, "Post Title is required"],
+  },
+  report_number: { type: String },
+  is_Bookmared: [
     {
-      timestamps: true,
-      toJSON: { virtuals: true },
-    
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  category: {
+    type: String,
+    // ref: "Category",
+   
+  },
+  // numViews: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "User",
+  //   },
+  // ],
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  // disLikes: [
+  //   {
+  //     type: mongoose.Schema.Types.ObjectId,
+  //     ref: "User",
+  //   },
+  // ],
 
-
-
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "Please Author is required"],
+  },
+  photo: {
+    type: String,
+    // required: [true, "Post Image is required"],
+  },
+},
+{
+  timestamps: true,
+  toJSON: { virtuals: true },
 })
 
 //hook
